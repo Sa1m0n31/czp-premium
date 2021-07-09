895c9a22390ad1ef133d2b2ffd93a443f0c50abe
Szymon Burak <sajmon0031@gmail.com>
2021-07-09 19:40:05 +0200

    Add AOS animations, end of pure html/css/js project

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Mobile menu */
 const openMenu = () => {
     const menu = document.querySelector(".topMenu__mobileMenu");
     const menuItems = document.querySelectorAll(".topMenu__mobileMenu>*");
@@ -19,3 +20,46 @@ const closeMenu = () => {
         menu.style.transform = "scaleX(0)";
     }, 400);
 }
+
+/* Sticky menu */
+const topMenu = document.querySelector(".topMenu");
+const topMenuLogo = document.querySelector(".topMenu__logo__img");
+const topMenuItems = document.querySelectorAll(".topMenu__list__item");
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 50) {
+        if(window.innerWidth > 768) topMenuLogo.style.width = "150px";
+        else topMenuLogo.style.width = "70px";
+        topMenuLogo.style.margin = "20px 0";
+        topMenu.style.borderBottom = "2px solid #CE2323";
+
+        topMenuItems.forEach(item => {
+            item.style.borderBottom = "none";
+        });
+    }
+    else {
+        if(window.innerWidth > 768) topMenuLogo.style.width = "250px";
+        else topMenuLogo.style.width = "70px";
+        topMenuLogo.style.margin = "0";
+        topMenu.style.borderBottom = "none";
+
+        topMenuItems.forEach(item => {
+            item.style.borderBottom = "1px solid #E1E1E1";
+        });
+    }
+});
+
+window.addEventListener("resize", () => {
+   if(window.innerWidth > 768) {
+       topMenuLogo.style.width = "250px";
+   }
+   else {
+       topMenuLogo.style.width = "70px";
+   }
+});
+
+/* AOS */
+AOS.init({
+    delay: 100,
+    duration: 600
+});
+
